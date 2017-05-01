a71059c1f48324097408da09bbef9ff19a5fe257
k_james <katelin.james@gmail.com>
2017-04-30 21:50:47 -0700

    add
    add

---
+++ part3/json.js
@@ -0,0 +1,27 @@
+{"teammembers":[
+	{
+		"name":"Agnes ",
+		"title":"Vice President of Accounting ",
+		"bio":"With over 10 years of experience "
+	},
+    {
+		"name":"Oscar ",
+		"title":"President ",
+		"bio":"With over 15 years of experience "
+	},
+	{
+		"name":"Kiki ",
+		"title":"Assistant ",
+		"bio":"With over 20 years of experience "
+	},
+    {
+		"name":"Katelin ",
+		"title":"Member ",
+		"bio":"With over 25 years of experience "
+	},
+    {
+		"name":"Apple ",
+		"title":"Volunteer ",
+		"bio":"With over 30 years of experience"
+	}
+]}
